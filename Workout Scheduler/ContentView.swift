b9802e588efed323291896909efc95f6f25ed428
Kevin Olmats <kevin.ee.olmats@gmail.com>
2021-07-07 17:31:11 -0600

    Initial Commit

---
+++ Workout Scheduler/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Workout Scheduler
+//
+//  Created by Kevin Olmats on 2021-07-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Workout Scheduler/Workout_SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_SchedulerApp.swift
+//  Workout Scheduler
+//
+//  Created by Kevin Olmats on 2021-07-07.
+//
+
+import SwiftUI
+
+@main
+struct Workout_SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
